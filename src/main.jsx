759e935a31169c449a6e1e56f9bdb4dd3aadbe10
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2023-11-25 22:22:53 +0600

    Axios, Tanstack query installation and useAxiosPublic hook part has been completed

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,12 @@ import Home from './Pages/Home/Home';
 import AuthProvider from './AuthProvider/AuthProvider';
 import SignUp from './Pages/SignUp/SignUp';
 import Login from './Pages/Login/Login';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -34,7 +40,16 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AuthProvider><RouterProvider router={router} /></AuthProvider>
-  </React.StrictMode>,
+  
+   <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+             <RouterProvider router={router} />
+          </AuthProvider>
+   
+      </QueryClientProvider>
+   </React.StrictMode>
+  ,
+
+
 )
