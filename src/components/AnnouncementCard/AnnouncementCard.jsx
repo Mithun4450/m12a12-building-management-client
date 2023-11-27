304953799029b81995173303cfa88c9977bfd2aa
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2023-11-27 15:26:34 +0600

    Annoncements part has been completed

---
+++ src/components/AnnouncementCard/AnnouncementCard.jsx
@@ -0,0 +1,22 @@
+
+
+const AnnouncementCard = ({announcement}) => {
+
+    const {title, date, description} = announcement;
+
+    return (
+        <div>
+            <div className="card bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="card-title text-center">{title}</h2>
+                    <p className="text-center">{date}</p>
+                    <p className="text-justify">{description}</p>
+                    
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default AnnouncementCard;
--- src/components/Announcements/Announcements.jsx
+++ src/components/Announcements/Announcements.jsx
@@ -1,11 +1,46 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import SectionTitle from "../SectionTitle/SectionTitle";
+import AnnouncementCard from "../AnnouncementCard/AnnouncementCard";
 
 
 const Announcements = () => {
-    return (
-        <div>
-            <h1>This is announcements page</h1>
+    const axiosSecure = useAxiosSecure();
+
+    const {data: announcements = [],  refetch} = useQuery({
+        queryKey: ['announcements'], 
+        queryFn: async() =>{
+            const res = await axiosSecure.get('/announcements');
+            return res.data;
             
+        }
+    })
+    console.log(announcements)
+
+    return (
+       
+        <div className="my-14 mx-6">
+            <SectionTitle 
+            subHeading={"Announcements & Insights"} 
+            heading={"Catch Up on Our Latest Developments and Announcements"}>
+            </SectionTitle>
+
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+            {
+                announcements.map(announcement => <AnnouncementCard key={announcement._id}
+                announcement={announcement}
+                ></AnnouncementCard>)
+            }
+            </div>
+
         </div>
+        
+
+        
+        
+        
+        
+        
     );
 };
 
